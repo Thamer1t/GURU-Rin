d6a182b3b6baba28041aa586da117bce72c9c9f4
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-14 01:56:33 +0300

    Update GURU-toanime.js

--- plugins/GURU-toanime.js
+++ plugins/GURU-toanime.js
@@ -2,8 +2,8 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
-if (!/image/g.test(mime)) throw '*Respond to a image*'
-m.reply('*This command can turn your photo into anime*')    
+if (!/image/g.test(mime)) throw '*رد على صورة*'
+m.reply('*هذا الأمر لتحويل صورة الشخص الى رسم انمي*')    
 let data = await q.download?.()
 let image = await uploadImage(data)
 try {
@@ -18,10 +18,10 @@ try{
 let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
 await conn.sendFile(m.chat, anime3, 'error.jpg', null, m) 
 } catch (e) {
-throw '*Error check if the persons face is visible*'
+throw '*لازم يكون وجه الشخص واضح*'
 }}}}
-handler.help = ["toanime"]
-handler.tags = ["tools"]
-handler.diamond = true
-handler.command = /^(imganime|toanime)$/i
+handler.help = ["انمي"]
+handler.tags = ["ادوات"]
+handler.diamond = false
+handler.command = /^(انمي|toanime)$/i
 export default handler
