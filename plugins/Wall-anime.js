8f8310a683c93aa03106ee96574cad308c28cbb7
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-14 02:24:29 +0300

    Update Wall-anime.js

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -5,6 +5,6 @@ let haha = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
 //conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 NEXT 🔄', `${usedPrefix + command}`]], m)    
 }
-handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
-handler.tags = ['anime']
+handler.command = handler.help = ['اكيرا', 'اكياما', 'انا', 'اسونا', 'ايوزاوا', 'بوروتو', 'تشيهو', 'شيتوغي', 'ديدارا', 'ايرزا', 'ايلينا', 'ايبا', 'ايميليا', 'هيستيا', 'هيناتا', 'اينوري', 'ايسوزو', 'ايتاشي', 'ايتوري', 'كاغا', 'كاغورا', 'كاوري', 'كانيكي', 'كوتوري', 'كورومي', 'مادارا', 'ميكاسا', 'ميكو', 'ميناتو', 'ناروتو', 'نيزوكو', 'ساغيري', 'ساسكي', 'ساكورا']
+handler.tags = ['خلفيات']
 export default handler
