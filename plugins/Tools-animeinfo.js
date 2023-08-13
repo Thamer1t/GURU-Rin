4bf0eee47351651276fcadfd97c11a3a17e05d80
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-14 02:16:22 +0300

    Update Tools-animeinfo.js

--- plugins/Tools-animeinfo.js
+++ plugins/Tools-animeinfo.js
@@ -5,11 +5,27 @@ const client = new Anime();
 
 let handler = async (m, { conn, text, usedPrefix }) => {
   if (!text) return m.reply(`*[❗] ادخل اسم الانمي اللي تبي تبحث عنه (بالانجليزي)*`);
-  try {
+ try {
     let anime = await client.searchAnime(text);
     let result = anime.data[0];
-    let resultes = await translate(`${result.background}`, { to: 'en', autoCorrect: true });
-    let resultes2 = await translate(`${result.synopsis}`, { to: 'hi', autoCorrect: true });
+
+    // Translate each piece of information to Arabic
+    let title = await translate(result.title, { to: 'ar' });
+    let type = await translate(result.type, { to: 'ar' });
+    let status = await translate(result.status.toUpperCase().replace(/\_/g, ' '), { to: 'ar' });
+    let episodes = await translate(result.episodes.toString(), { to: 'ar' });
+    let duration = await translate(result.duration, { to: 'ar' });
+    let source = await translate(result.source.toUpperCase(), { to: 'ar' });
+    let airedFrom = await translate(result.aired.from, { to: 'ar' });
+    let airedTo = await translate(result.aired.to, { to: 'ar' });
+    let popularity = await translate(result.popularity.toString(), { to: 'ar' });
+    let favorites = await translate(result.favorites.toString(), { to: 'ar' });
+    let rating = await translate(result.rating.toString(), { to: 'ar' });
+    let rank = await translate(result.rank.toString(), { to: 'ar' });
+    let trailerUrl = await translate(result.trailer.url, { to: 'ar' });
+    let background = await translate(result.background, { to: 'ar' });
+    let synopsis = await translate(result.synopsis, { to: 'ar' });
+
     let AnimeInfo = `
 🎀 • *الاسم:* ${result.title}
 🎋 • *االصيغة:* ${result.type}
@@ -24,7 +40,6 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 🎇 • *التقييم:* ${result.rating}
 🏅 • *الترتيب:* ${result.rank}
 ♦ • *تريلر:* ${result.trailer.url}
-🌐 • *URL:* ${result.url}
 🎆 • *خلفية:* ${resultes.text}
 ❄ • *الملخص:* ${resultes2.text}`;
 
