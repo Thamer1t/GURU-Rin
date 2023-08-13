e4fd3b330530a04f7acb7d4bb8ea6af01b6eb74b
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-14 02:30:28 +0300

    Update Wall-anime.js

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+/*import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/anime-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]   
@@ -8,3 +8,4 @@ conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
 handler.command = handler.help = ['اكيرا', 'اكياما', 'انا', 'اسونا', 'ايوزاوا', 'بوروتو', 'تشيهو', 'شيتوغي', 'ديدارا', 'ايرزا', 'ايلينا', 'ايبا', 'ايميليا', 'هيستيا', 'هيناتا', 'اينوري', 'ايسوزو', 'ايتاشي', 'ايتوري', 'كاغا', 'كاغورا', 'كاوري', 'كانيكي', 'كوتوري', 'كورومي', 'مادارا', 'ميكاسا', 'ميكو', 'ميناتو', 'ناروتو', 'نيزوكو', 'ساغيري', 'ساسكي', 'ساكورا']
 handler.tags = ['خلفيات']
 export default handler
+*/
