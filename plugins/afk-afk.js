1dca81997c987c8c45b387398a9609ff7bc8ce98
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-14 03:12:17 +0300

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,5 +1,5 @@
 //import db from '../lib/database.js'
-
+/*
 let handler = async (m, { text, conn }) => {
     let user = global.db.data.users[m.sender]
     user.afk = + new Date
@@ -17,3 +17,4 @@ handler.command = ['afk']
 handler.group = true
 
 export default handler
+*/
